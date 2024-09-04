fca004f9c834c8105edc597d4cb1626786bdf9ed
Max <47777521+DKAMX@users.noreply.github.com>
2024-09-04 20:47:05 +1200

    feat: attribute reader
    For extracting attribute name, value

---
+++ seria_attribute.py
@@ -0,0 +1,54 @@
+import re
+import sys
+
+
+class AttributeReader:
+    def __init__(self, path):
+        self.file = open(path, 'r', encoding='cp1252')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.file.close()
+
+    def listAllAttributeName(self):
+        matching = self.matchPattern(r'[a-zA-Z][0-9a-zA-Z.:_]*=')
+
+        name_set = set()
+        for match_object in matching:
+            # -1 to remove '='
+            name_set.add(match_object.group()[:-1])
+
+        return sorted(name_set)
+
+    def listAttributeValues(self, attribute_name):
+        # make content after '=' to be a match group
+        matching = self.matchPattern(attribute_name + r'=(.*)')
+
+        value_set = set()
+        for match_object in matching:
+            value_set.add(match_object.group(1))
+
+        return sorted(value_set)
+
+    def matchPattern(self, pattern):
+        matching = list()
+
+        for line in self.file:
+            match_object = re.match(pattern, line)
+            if match_object:
+                matching.append(match_object)
+
+        return matching
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python seria_attribute.py <seria_file>')
+        sys.exit(1)
+
+    with AttributeReader(sys.argv[1]) as reader:
+        print('Attribute names:')
+        for name in reader.listAllAttributeName():
+            print(name)
