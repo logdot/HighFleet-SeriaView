cb7be68ff565e494ac0b32371392759216caa14f
Max <47777521+DKAMX@users.noreply.github.com>
2024-10-11 14:30:46 +1300

    remove obsolete functions
    The SeriaTree needs another refactor, see #1

--- seria_cli.py
+++ seria_cli.py
@@ -2,14 +2,15 @@ import logging
 import sys
 import seria
 
+__author__ = 'Max'
+__version__ = '0.1.0'
+
 
 def _print_help():
     print('''Usage: python seria.py [option] <seria_files...>
 Options:
     -attributes              | List all distinct attributes
     -values <attribute name> | List all distinct values for the given attribute
-    -tree                    | Print the tree representation with depth 3
-    -json                    | Convert the given seria files to JSON
 Example:
     python seria.py -values m_classname profile.seria parts.seria''')
 
@@ -31,26 +32,11 @@ def process_files_by_line(filepaths, process_line):
             logging.error('Could not open file: ' + filepath)
 
 
-def process_files_by_tree(filepaths, process, depth=None):
-    '''Process files with the given tree processing function'''
-
-    for filepath in filepaths:
-        try:
-            tree = seria.load(filepath, depth)
-            if tree is None:
-                logging.error(f'Could not open file: {filepath}')
-                continue
-
-            yield filepath, process(tree)
-        except IOError:
-            logging.error(f'Could not process file: {filepath}')
-
-
 def list_attributes(filepaths):
     '''List all distinct attributes in the given files'''
 
     def process_line(line, result_set):
-        attribute, _ = seria._matchAttribute(line)
+        attribute, _ = seria._match_attribute(line)
         if attribute is not None:
             result_set.add(attribute)
 
@@ -64,7 +50,7 @@ def list_values(attribute_name, filepaths):
     '''List all distinct values for the given attribute in the given files'''
 
     def process_line(line, result_set):
-        attribute, value = seria._matchAttribute(line)
+        attribute, value = seria._match_attribute(line)
         if attribute == attribute_name:
             result_set.add(value)
 
@@ -72,26 +58,6 @@ def list_values(attribute_name, filepaths):
         print(f'Values for attribute {attribute_name} in file {filepath}:')
         for value in sorted(value_set):
             print(value)
-
-
-def print_seria_tree_file(filepaths):
-    '''Print the tree representation of the given files with depth 3'''
-
-    for filepath, output in process_files_by_tree(filepaths, lambda tree: tree.to_text(), 3):
-        file = open(filepath + '.tree.txt', 'w')
-        file.write(output)
-        file.close()
-        print(f'Tree representation for file {filepath} save to {file.name}')
-
-
-def print_seria_json_file(filepaths):
-    '''Convert the given seria files to JSON'''
-
-    for filepath, output in process_files_by_tree(filepaths, lambda tree: tree.to_json()):
-        file = open(filepath + '.json', 'w')
-        file.write(output)
-        file.close()
-        print(f'JSON representation for file {filepath} save to {file.name}')
 
 
 if __name__ == '__main__':
@@ -112,10 +78,6 @@ if __name__ == '__main__':
             sys.exit(1)
 
         list_values(sys.argv[2], sys.argv[3:])
-    elif option == '-tree':
-        print_seria_tree_file(sys.argv[2:])
-    elif option == '-json':
-        print_seria_json_file(sys.argv[2:])
     else:
         logging.error(f'Invalid option: {option}')
         _print_help()
