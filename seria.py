5842ea715b3c7e8e56ca8fe63c3c4d91d6370a0e
Max <47777521+DKAMX@users.noreply.github.com>
2024-10-12 00:02:42 +1300

    refactor: another attempt to model seria #1
    alist is a custom type that combines dict and list. This allow us to maintain the order of nodes and attributes. And produce exactly matched output.

--- seria.py
+++ seria.py
@@ -1,7 +1,6 @@
 import logging
 import logging.config
 import re
-from treelib import Node, Tree
 
 __author__ = 'Max'
 __version__ = '0.1.0'
@@ -9,184 +8,88 @@ __version__ = '0.1.0'
 ATTRIBUTE_PATTERN = r'([a-zA-Z][0-9a-zA-Z.:_]*)'
 VALUE_PATTERN = r'(.*)'
 
-
-class SeriaNode(Node):
-    def __init__(self, tag=None, identifier=None, expanded=True, data=None):
-        super().__init__(tag, identifier, expanded, data)
-
-    def has_attribute(self, attribute_name):
-        return attribute_name in self.data
-
-    def get_attribute(self, attribute_name):
-        '''Get the value of an attribute. Return None if the attribute not exist.'''
-        return self.data[attribute_name] if attribute_name in self.data else None
-
-    def set_attribute(self, attribute_name, value):
-        '''Set the value of an attribute. If the attribute not exist, it will be created.'''
-        self.data[attribute_name] = value
-
-    def del_attribute(self, attribute_name):
-        '''Remove an attribute. Return the value of the attribute if it exists.'''
-        return self.data.pop(attribute_name, None)
-
-    def get_attribute_names(self):
-        return list(self.data.keys())
-
-    def update_attribute_by_value(self, previous_value, new_value):
-        '''Update the attribute value.
-        If the attribute value is equal to the previous value, it will be updated to the new value.'''
-
-        for name in self.get_attribute_names():
-            if self.get_attribute(name) == previous_value:
-                self.set_attribute(name, new_value)
+# logging.basicConfig(level=logging.DEBUG)
 
 
-class SeriaTree(Tree):
-    def __init__(self, tree=None, deep=False, node_class=SeriaNode, identifier=None):
-        super().__init__(tree, deep, node_class, identifier)
+class alist:
+    '''Association list that maintains the order of key-value pairs and allows insertion.
+    The dict part is used to store the data, while the list part is used to store the order of insertion.'''
 
-    def _root_node(self) -> SeriaNode:
-        return self.get_node(self.root)
+    def __init__(self, data: dict = None):
+        self.data = data or dict()
+        self.order = list(data.keys())
 
-    def to_json(self) -> str:
-        return super().to_json(with_data=True, sort=False)
+    def __iter__(self):
+        for key in self.order:
+            yield key, self.get(key)
 
-    def to_text(self) -> str:
-        '''Text representation of the tree structure.'''
-        return self.show(stdout=False, sorting=False)
+    def __contains__(self, key) -> bool:
+        return key in self.keys()
 
-    # root node operation
-    def get_attribute(self, attribute_name):
-        '''Get the attribute value of the root node. Return None if the attribute not exist.'''
-        root_node = self._root_node()
-        return root_node.data[attribute_name] if attribute_name in root_node.data else None
+    # list operations
+    def insert(self, index, key, value):
+        self.data[key] = value
+        self.order.insert(index, key)
 
-    def set_attribute(self, attribute_name, value):
-        '''Set the attribute value of the root node. If the attribute not exist, it will be created.'''
-        self._root_node().data[attribute_name] = value
+    def remove(self, key):
+        del self.data[key]
+        self.order.remove(key)
 
-    def del_attribute(self, attribute_name):
-        '''Remove an attribute from the root node. Return the value of the attribute if it exists.'''
-        return self._root_node().data.pop(attribute_name, None)
+    def index(self, key) -> int:
+        return self.order.index(key)
 
-    def get_attribute_names(self):
-        return list(self._root_node().data.keys())
+    # dict operations
+    def get(self, key):
+        return self.data.get(key)
 
-    def update_attribute(self, attribute_name, value):
-        '''Update the attribute value of the root node.
-        It will also find and update the attribute in the child that reference the previous value.
-        This is different from set_attribute, which only update existing attribute.'''
+    def keys(self):
+        return set(self.order)
 
-        previous_value = self.get_attribute(attribute_name)
+    def put(self, key, value):
+        self.data[key] = value
+        if key not in self.order:
+            self.order.append(key)
 
-        if not self._root_node().has_attribute(attribute_name):
+
+class SeriaNode:
+    def __init__(self, header: str, classname: str):
+        self.header = header
+        self.data_group = list()
+        self.data_group.append(alist({'m_classname': classname}))
+
+    def _add_attribute(self, name: str, value: str):
+        last_group = self.data_group[-1]
+        if not isinstance(last_group, alist):
+            self.data_group.append(alist({name: value}))
+        else:
+            if name in last_group:
+                existing_value = last_group.get(name)
+                if isinstance(existing_value, list):
+                    last_group.put(name, existing_value + [value])
+                else:
+                    last_group.put(name, [existing_value, value])
+            else:
+                last_group.put(name, value)
+
+    def _add_child(self, node):
+        if not isinstance(node, SeriaNode):
+            logging.error('Child must be a SeriaNode')
             return
+        self.data_group.append(node)
 
-        self._root_node().set_attribute(attribute_name, value)
-
-        for node in self.all_nodes():
-            node.update_attribute_by_value(previous_value, value)
-
-    # child node operation
-    def get_node_by_index(self, index: int) -> SeriaNode:
-        '''Get a child node by its order(index) in the file.'''
-        return self.get_nodes()[index]
-
-    def get_node_if(self, predicate) -> SeriaNode:
-        '''Get the first child node that satisfy the predicate function.'''
-
-        for child in self.children(self.root):
-            if predicate(child):
-                return child
+    # node read operations
+    def get_attribute(self, name: str):
+        for group in self.data_group:
+            if isinstance(group, alist):
+                # assume that attribute only appear in consecutive order and will not spread accross groups
+                if name in group.keys():
+                    return group.get(name)
         return None
 
-    def get_node_by_class(self, type: str) -> SeriaNode:
-        '''Get the first child node with the specified classname.'''
-        return self.get_node_if(lambda node: node.tag == type)
 
-    # child nodes read operation
-    def get_nodes(self) -> list:
-        '''Get all direct child nodes of the root node.'''
-        return self.children(self.root)
-
-    def get_nodes_by_class(self, type: str) -> filter:
-        '''Get all direct child nodes of the root node with the specified type.'''
-        return self.filter_nodes(lambda node: node.tag == type)
-
-    def map_nodes(self, mapper) -> list:
-        '''Returns a result list of applying the mapper function to all child nodes.'''
-        return [mapper(child) for child in self.children(self.root)]
-
-    def node_classes(self) -> set:
-        '''Get all direct child node types of the root node.'''
-        return set(self.map_nodes(lambda node: node.tag))
-
-    def get_children_attribute(self, attribute_name) -> list:
-        '''Get the attribute value of all child nodes if the attribute exists.'''
-        return [node.get_attribute(attribute_name) for node in self.filter_nodes(lambda node: node.has_attribute(attribute_name))]
-
-    # child nodes write operation
-    def add_nodes(self, nodes: list):
-        for node in nodes:
-            self.add_node(node, self._root_node())
-
-    def del_node_by_index(self, index: int):
-        '''Remove a child node by its order(index) in the file.'''
-        nodes = self.get_nodes()
-        self.remove_node(nodes[index].identifier)
-
-    def del_nodes(self, nodes: list) -> int:
-        '''Remove all child nodes in the list. Return the number of nodes removed.'''
-
-        count = 0
-        for node in nodes:
-            count += self.remove_node(node)
-        return count
-
-    def del_nodes_if(self, predicate) -> int:
-        '''Remove all child nodes that satisfy the predicate (condition) function. Return the number of nodes removed.'''
-
-        nodes = [child for child in self.children(
-            self.root) if predicate(child)]
-        return self.del_nodes(nodes)
-
-    def del_nodes_by_class(self, classname: str) -> int:
-        '''Remove all child nodes with the specified classname. Return the number of nodes removed.'''
-        return self.del_nodes_if(lambda node: node.tag == classname)
-
-    def foreach_node(self, action):
-        '''Apply the action function to all child nodes.'''
-        for child in self.children(self.root):
-            action(child)
-
-    # child tree operation
-    def get_subtree_by_index(self, index: int):
-        '''Get a subtree (shallow) by its order(index) in the file.'''
-        return self.subtree(self.get_node_by_index(index).identifier)
-
-    def get_subtree_if(self, predicate):
-        '''Get the first subtree that satisfy the predicate function.'''
-
-        for child in self.children(self.root):
-            if predicate(child):
-                return self.subtree(child.identifier)
-        return None
-
-    def get_subtree_by_class(self, classname: str):
-        '''Get the first subtree with the specified name.'''
-        return self.get_subtree_if(lambda node: node.tag == classname)
-
-    def get_subtrees_if(self, predicate):
-        '''Get all subtrees that satisfy the predicate function.'''
-        return [self.subtree(child.identifier) for child in self.children(self.root) if predicate(child)]
-
-    def get_subtrees_by_class(self, classname: str):
-        '''Get all subtrees with the specified name.'''
-        return self.get_subtrees_if(lambda node: node.tag == classname)
-
-
-def _matchAttribute(input: str):
-    '''Match an attribute and its value from a line of text. Return a tuple of the attribute and its value.'''
+def _match_attribute(input: str):
+    '''Match an attribute and its value from a line of text.
+    @return: a tuple of the attribute and its value, or None if no match'''
 
     match_result = re.match(ATTRIBUTE_PATTERN + '=' + VALUE_PATTERN, input)
     if match_result:
@@ -194,112 +97,98 @@ def _matchAttribute(input: str):
     return None, None
 
 
-def dump(tree: SeriaTree) -> str:
-    '''Dump the tree back to the seria format.'''
-
-    root = tree._root_node()
+def _dump(node: SeriaNode) -> str:
+    '''Dump a SeriaNode to a string.'''
 
     output = []
-    header = root.data['_header']
-    if header:
-        output.append(header)
 
+    if node.header is not None:
+        output.append(node.header)
     output.append('{')
 
-    for key, value in root.data.items():
-        if key == '_header':
-            continue
-        elif key == '_meshData':
-            for item in value:
-                output.append(item)
-        elif isinstance(value, list):
-            for item in value:
-                output.append(f'{key}={item}')
+    for group in node.data_group:
+        if isinstance(group, alist):
+            for name, value in group:
+                if name == '_mesh':
+                    output.extend(f'{v}' for v in value)
+                else:
+                    if isinstance(value, list):
+                        output.extend(f'{name}={v}' for v in value)
+                    else:
+                        output.append(f'{name}={value}')
         else:
-            output.append(f'{key}={value}')
-
-    for child in tree.children(root.identifier):
-        output.append(dump(tree.subtree(child.identifier)))
+            output.append(_dump(group))
 
     output.append('}')
-
     return '\n'.join(output)
 
 
-def load(filepath: str, max_depth: int = None) -> SeriaTree:
-    '''Load a seria file and return a tree structure.
-    load function uses original treelib functions to create and manage the tree structure.
-    Beaware that SeriaTree is a subclass of Tree, and may have overidden methods.
-    @param filepath: The path to the seria file.
-    @param max_depth: The maximum depth of the tree. If None, the tree will be fully parsed.
-    @return: The tree structure or None if the file could not be opened.'''
+def dump(filepath: str, node: SeriaNode):
+    '''Dump a SeriaNode to a file.'''
 
-    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('seria.dump')
+
+    try:
+        with open(filepath, 'w', encoding='cp1251') as file:
+            file.write(_dump(node) + '\n')
+    except IOError:
+        logger.error(f'Could not open file: {filepath}')
+
+
+def load(filepath: str) -> SeriaNode:
+    '''Load a SeriaNode from a file.
+    @return: the root node of the SeriaNode, or None if the file could not be opened'''
+
+    logger = logging.getLogger('seria.load')
 
     try:
         with open(filepath, 'r', encoding='cp1251') as file:
-            input_lines = file.readlines()
+            lines = file.readlines()
     except IOError:
-        logging.error(f'Could not open file: {filepath}')
+        logger.error(f'Could not open file: {filepath}')
         return None
 
-    tree = SeriaTree()
-    depth = 0
-    classname = None
-    parent_ids = []
-    previous_line = None
+    parent_nodes = list()
+    header_line = None
+    node = None
 
-    for line_index, line in enumerate(input_lines):
+    for index, line in enumerate(lines):
         line = line.strip()
 
         if line == '{':
-            depth += 1
+            continue
         elif line == '}':
-            if max_depth is None or depth <= max_depth:
-                parent_ids.pop()
-            depth -= 1
+            # the last '}' will give the last node which is the root node and return it at the end
+            node = parent_nodes.pop()
         else:
-            if max_depth and depth > max_depth:
-                continue
-
-            next_line_index = line_index + 1
-            if next_line_index < len(input_lines):
-                # the line before the curly brace is belong the next node
-                # e.g. 'm_escadras=327' is belong to node 'Escadra'
-                next_line = input_lines[next_line_index].strip()
+            # the line before the curly brace is belong the next node
+            # e.g. 'm_escadras=327' is belong to node 'Escadra'
+            next_index = index + 1
+            if next_index < len(lines):
+                next_line = lines[next_index].strip()
                 if next_line == '{':
-                    previous_line = line
+                    header_line = line
                     continue
 
-            attribute_name, attribute_value = _matchAttribute(line)
+            name, value = _match_attribute(line)
 
-            if attribute_name is None:
-                # handle the case where the line is a value with no attribute name
-                if classname == 'Mesh':
-                    node = tree.get_node(parent_ids[-1])
-                    node.data.setdefault('_meshData', []).append(line)
+            if name == 'm_classname':
+                logger.info(f'new node: {value}')
+
+                node = SeriaNode(header_line, value)
+
+                if len(parent_nodes) > 0:
+                    parent_nodes[-1]._add_child(node)
+
+                parent_nodes.append(node)
+            elif name is None:
+                node = parent_nodes[-1]
+
+                if node.data_group[0].get('m_classname') == 'Mesh':
+                    node._add_attribute('_mesh', line)
             else:
-                # it is safe to skip empty attribute values
-                if attribute_value == '':
-                    continue
+                logger.debug(f'new attribute: {name} = {value}')
 
-                if attribute_name == 'm_classname':
-                    # create a new node, always starts with m_classname
-                    classname = attribute_value
-                    initial_data = {'_header': previous_line.strip() if previous_line else None,
-                                    'm_classname': classname}
-                    node = tree.create_node(
-                        classname, parent=parent_ids[-1] if parent_ids else None, data=initial_data)
-                    parent_ids.append(node.identifier)
-                else:
-                    node = tree.get_node(parent_ids[-1])
-                    if attribute_name in node.data:
-                        if isinstance(node.data[attribute_name], list):
-                            node.data[attribute_name].append(attribute_value)
-                        else:
-                            node.data[attribute_name] = [
-                                node.data[attribute_name], attribute_value]
-                    else:
-                        node.data[attribute_name] = attribute_value
+                parent_nodes[-1]._add_attribute(name, value)
 
-    return tree
+    return node
